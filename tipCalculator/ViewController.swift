2c5412978e90f57e740daac565738a45fcf1de77
Kabsa <kabsaabdi23@gmail.com>
2021-01-05 16:05:29 -0700

    Initial Commit

---
+++ tipCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tipCalculator
+//
+//  Created by Kabsa Abdi on 1/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
